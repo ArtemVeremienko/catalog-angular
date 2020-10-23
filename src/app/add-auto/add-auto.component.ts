f6b39e75ace74d85657f8deb8760a285024e60c0
Artem <temaspsxxx@gmail.com>
2020-10-23 19:58:06 +0300

    feat: add create auto component

---
+++ src/app/add-auto/add-auto.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-auto',
+  templateUrl: './add-auto.component.html',
+  styleUrls: ['./add-auto.component.scss']
+})
+export class AddAutoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
