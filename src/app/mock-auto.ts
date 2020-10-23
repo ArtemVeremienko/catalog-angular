609374814e1cc0dc6bae1aa532d8328bf1dd7b7d
Artem <temaspsxxx@gmail.com>
2020-10-23 19:56:00 +0300

    feat: add mock autos

---
+++ src/app/mock-auto.ts
@@ -0,0 +1,25 @@
+import { Auto } from './interfaces'
+
+export const AUTOS: Auto[] = [
+  {
+    id: 1,
+    image: 'img.png',
+    name: 'BMW',
+    description: 'More expensive, less quality',
+    year: 2018
+  },
+  {
+    id: 2,
+    image: 'img2.png',
+    name: 'Mercedes',
+    description: 'Germany style',
+    year: 2019
+  },
+  {
+    id: 3,
+    image: 'img3.png',
+    name: 'Audi',
+    description: 'Four circles - four tires',
+    year: 2017
+  },
+]
