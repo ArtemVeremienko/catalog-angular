98954b21f21aaaba3c2b6e28c81540d71a389348
Artem <temaspsxxx@gmail.com>
2020-10-23 19:56:18 +0300

    feat: add interfaces

---
+++ src/app/interfaces.ts
@@ -0,0 +1,13 @@
+
+export interface User {
+  login: string;
+  password: string;
+}
+
+export interface Auto {
+  id: number;
+  image: string;
+  name: string;
+  description: string;
+  year: number;
+}
