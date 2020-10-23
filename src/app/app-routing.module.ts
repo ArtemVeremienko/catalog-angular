ca8a2a5a5b560dcf67d7dcc7587fcd125da0c206
Artem <temaspsxxx@gmail.com>
2020-10-23 21:46:35 +0300

    feat: add routes paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component'
+import { AutoListComponent } from './auto-list/auto-list.component'
+import { AddAutoComponent } from './add-auto/add-auto.component'
+import { AutoInfoComponent } from './auto-info/auto-info.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'cars', component: AutoListComponent },
+  { path: 'cars/:id', component: AutoInfoComponent },
+  { path: 'add', component: AddAutoComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
